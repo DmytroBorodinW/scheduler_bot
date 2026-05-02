3d143cf64dec8abe96fb2f510a84c74f3f6f954f
DmytroBorodinW <dmytro.borodin.ghb@gmail.com>
2026-05-02 17:00:11 +0300

    fix: update index.ts and yml. Task: TS-1

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,8 +1,8 @@
-import * as dotenv from "dotenv";
+// import * as dotenv from "dotenv";
 import "dotenv/config";
 import { Telegraf } from "telegraf";
 
-dotenv.config();
+// dotenv.config();
 const TOKEN = process.env.BOT_TOKEN;
 
 export const bot = new Telegraf(TOKEN ? TOKEN : "");
